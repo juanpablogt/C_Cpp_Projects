f2bdba972c829e47641a8969ff84f39802b13567
JPablo <juapabgonzalez@utp.edu.co>
2023-07-21 23:16:06 -0500

    hola mundo2

--- c_c++/hola.cpp
+++ c_c++/hola.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hola mundo!*100" << std::endl;
+    std::cout << "Hola mundo!*1" << std::endl;
+    std::cout << "Hola mundo!*2" << std::endl;
     return 0;
 }
